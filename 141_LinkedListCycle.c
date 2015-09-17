f644cd24207ccd9715bf79409c0491f542e44484
bashell <nju.liuc@gmail.com>
2015-09-17 10:57:52 +0800

    AC

---
+++ 141_LinkedListCycle.c
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+bool hasCycle(struct ListNode *head) {
+    if(head == NULL || head -> next == NULL)
+        return false;
+        
+    struct ListNode *fast, *slow;
+    fast = head -> next;
+    slow = head;
+    while(fast != slow){
+        if(fast == NULL || fast -> next == NULL)
+            return false;
+        fast = fast -> next -> next;
+        slow = slow -> next;
+    }
+    return true;
+}
