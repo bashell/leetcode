17bd5c9a03543df16e917cd58993eff868323d99
bashell <nju.liuc@gmail.com>
2015-09-20 01:48:56 +0800

    AC

--- 69_Sqrt.c
+++ 69_Sqrt.c
@@ -4,7 +4,7 @@ int mySqrt(int x)
     
 	if(x < 2)
 	    return x;
-    while(result * result > x)    // Å£¶Ùµü´ú·¨
+    while(result * result > x)  // Newton's method
         result = (result + x / result) >> 1;
     
     return result;
