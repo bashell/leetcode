ee291dcf54bc2b94874f4d73054757d9aeb39be7
bashell <nju.liuc@gmail.com>
2015-09-25 12:52:14 +0800

    Linked List

---
+++ 203_RemoveLinkedListElements.c
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* removeElements(struct ListNode* head, int val) {
+    if(head == NULL || head -> next == NULL && head -> val == val)
+        return NULL;
+    struct ListNode *pre = NULL;
+    struct ListNode *newHead = (struct ListNode *)malloc(sizeof(struct ListNode *));
+    if(newHead == NULL) {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+    newHead -> val = val - 1;
+    newHead -> next = head;
+
+    head = newHead;
+    pre = newHead;
+    while(head != NULL) {
+        if(head -> val == val) {
+            head = head -> next;
+            pre -> next = head;
+        }else{
+            pre = head;
+            head = head -> next;
+        }
+    }
+    head = newHead -> next;
+    free(newHead);
+    return head;
+}
