e54ca943b814b1fdadba8850527ab2535cb44a92
bashell <nju.liuc@gmail.com>
2015-09-27 10:57:39 +0800

    Bit Manipulation

--- 169_MajorityElement.cpp
+++ 169_MajorityElement.cpp
@@ -4,17 +4,17 @@
 using namespace std;
 
 class Solution{
-    public:
-        int majorityElement(vector<int>& nums){
-            map<int, int> elements;
-            for(vector<int>::iterator it = nums.begin();
-                    it != nums.end(); ++it)    // 统计每个元素出现的次数
-                ++elements[*it];
-            for(map<int, int>::iterator iter = elements.begin();
-                    iter != elements.end(); ++iter)    // 遍历关联容器
-                if(iter -> second > nums.size() / 2)
-                    return iter -> first;
-        }
+public:
+    int majorityElement(vector<int>& nums) {
+        map<int, int> elements;
+        for(vector<int>::iterator it = nums.begin();
+                it != nums.end(); ++it)    // 统计每个元素出现的次数
+            ++elements[*it];
+        for(map<int, int>::iterator iter = elements.begin();
+                iter != elements.end(); ++iter)    // 遍历关联容器
+            if(iter -> second > nums.size() / 2)
+                return iter -> first;
+    }
 };
 
 int main()
@@ -28,6 +28,5 @@ int main()
     nums.push_back(2);
     nums.push_back(3);
     cout << "The majority element is: " << solu.majorityElement(nums) << endl;
-
     return 0;
 }
--- 268_MissingNumber.cpp
+++ 268_MissingNumber.cpp
@@ -1,9 +1,9 @@
 class Solution{
-    public:
-        int missingNumber(vector<int>& nums){
-            int misNum = 0, len = nums.size();
-            for(int i = 0; i < len; ++i)
-                misNum ^= i ^ nums[i];    // x与自身异或为0, x与0异或为x
-            return misNum ^ len;
-        }
+public:
+    int missingNumber(vector<int>& nums) {
+        int misNum = 0, len = nums.size();
+        for(int i = 0; i < len; ++i)
+            misNum ^= i ^ nums[i];    // x ^ x = 0, x ^ 0 = x
+        return misNum ^ len;
+    }
 };
