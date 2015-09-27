fc71b2316acfac300d345e05852bbb3f182d0bd9
bashell <nju.liuc@gmail.com>
2015-09-27 10:43:25 +0800

    Bit Manipulation

--- 136_SingleNumber.c
+++ 136_SingleNumber.c
@@ -1,14 +1,11 @@
 int singleNumber(int *nums, int numsSize)
 {
 	int temp = *nums;
-	int *ptr;
+	int *ptr = NULL;
 
     if(numsSize == 1)
 		return temp;
-	else
-	{
-		for(ptr = nums + 1; ptr < nums + numsSize; ++ptr)
-			temp ^= *ptr;
-		return temp;
-	}
+    for(ptr = nums + 1; ptr < nums + numsSize; ++ptr)
+		temp ^= *ptr;
+	return temp;
 }
