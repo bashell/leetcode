0471bf1f8114b749b5c7ba22cbe7ef5943baa56f
bashell <nju.liuc@gmail.com>
2015-09-28 20:40:07 +0800

    Binary Tree

--- 226_InvertBinaryTree.c
+++ 226_InvertBinaryTree.c
@@ -10,15 +10,12 @@ struct TreeNode* invertTree(struct TreeNode* root) {
     if(root == NULL)
         return NULL;    
     struct TreeNode *temp;
-
     // 交换左右子树
     temp = root -> left;
     root -> left = root -> right;
     root -> right = temp;
-    
     // 递归
     invertTree(root -> left);
     invertTree(root -> right);
-    
     return root;
 }
