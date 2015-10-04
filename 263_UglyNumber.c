f6f7acb7f871e176bb7b0983e568917b340a0fa6
bashell <nju.liuc@gmail.com>
2015-10-04 23:12:20 +0800

    Math

--- 263_UglyNumber.c
+++ 263_UglyNumber.c
@@ -1,8 +1,8 @@
-bool isUgly(int num){
+bool isUgly(int num) {
     if(num <= 0)
         return false;
     while(num % 2 == 0)
-        num >>= 1;
+        num /= 2;
     while(num % 3 == 0)
         num /= 3;
     while(num % 5 == 0)
