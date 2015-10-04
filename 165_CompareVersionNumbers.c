2736a037a91430246f0464996fcb075983f64fb7
bashell <nju.liuc@gmail.com>
2015-10-04 22:38:51 +0800

    String

--- 165_CompareVersionNumbers.c
+++ 165_CompareVersionNumbers.c
@@ -2,7 +2,7 @@ int compareVersion(char* version1, char* version2) {
     char *ptr1 = version1, *ptr2 = version2;
     int val1 = 0, val2 = 0;
     
-    while(*ptr1 != '\0' || *ptr2 != '\0'){
+    while(*ptr1 != '\0' || *ptr2 != '\0') {
         // 跳过小数点
         if(*ptr1 == '.')
             ++ptr1;
@@ -10,19 +10,19 @@ int compareVersion(char* version1, char* version2) {
             ++ptr2;
             
         // 分别计算两个版本对应的子版本号
-        while(*ptr1 != '\0' && *ptr1 != '.'){
+        while(*ptr1 != '\0' && *ptr1 != '.') {
             val1 *= 10;
             val1 += *ptr1 - '0';
             ++ptr1;
         }
-        while(*ptr2 != '\0' && *ptr2 != '.'){
+        while(*ptr2 != '\0' && *ptr2 != '.') {
             val2 *= 10;
             val2 += *ptr2 - '0';
             ++ptr2;
         }
         
         // 比较版本号大小
-        if(val1 > val2)
+        if(val1 > val2) 
             return 1;
         else if(val1 < val2)
             return -1;
