f221d2242b89245779e628e86872f5adeb98dcc7
bashell <nju.liuc@gmail.com>
2015-10-04 22:56:16 +0800

    Hash Table

--- 1_TwoSum.cpp
+++ 1_TwoSum.cpp
@@ -1,17 +1,14 @@
 class Solution{
     public:
-        vector<int> twoSum(vector<int> &nums, int target)
-        {
+        vector<int> twoSum(vector<int> &nums, int target) {
             unordered_map<int, int> mapping;
             vector<int> result;
-            for(int i = 0; i < nums.size(); i++)
+            for(int i = 0; i < nums.size(); ++i)
                 mapping[nums[i]] = i;
         
-            for(int i = 0; i < nums.size(); i++)
-            {
+            for(int i = 0; i < nums.size(); i++) {
                 const int gap = target - nums[i];
-                if(mapping.find(gap) != mapping.end() && mapping[gap] > i)
-                {
+                if(mapping.find(gap) != mapping.end() && mapping[gap] > i) {
                     result.push_back(i + 1);
                     result.push_back(mapping[gap] + 1);
                     break;
