5538f7aa4c4d6257a156a70ecc6657fcb182ec6d
bashell <nju.liuc@gmail.com>
2015-10-04 23:08:08 +0800

    Hash Table

--- 217_ContainsDuplicate.cpp
+++ 217_ContainsDuplicate.cpp
@@ -2,10 +2,9 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         map<int, int> elements;
-        for(int i = 0; i != nums.size(); ++i)     // 将vector中的元素拷贝到map容器中, 形成pair<const int, int>
+        for(int i = 0; i != nums.size(); ++i)
             ++elements[nums[i]];
-        for(map<int, int>::iterator iter = elements.begin();    // 遍历map, 查看是否满足条件
-                iter != elements.end(); ++iter)
+        for(map<int, int>::iterator iter = elements.begin(); iter != elements.end(); ++iter)
             if(iter -> second >= 2)
                 return true;
         return false;
