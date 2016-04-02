9ec4d1cc4587757eff86437fa9686cf48360f622
bashell <nju.liuc@gmail.com>
2016-04-02 11:58:39 +0800

    Math

--- 171_ExcelSheetColumnNumber.cpp
+++ 171_ExcelSheetColumnNumber.cpp
@@ -1,25 +1,11 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-class Solution{
+class Solution {
 public:
     int titleToNumber(string s) {
         int num = 0;
-        for(string::size_type ix = 0; ix < s.size(); ++ix) {
-            num *= 26;    // 26进制:每左移一位,都乘以26
-            num += s[ix] - 'A' + 1;
+        for(auto c : s) {
+            num *= 26;    // 26进制：每左移一位，都乘以26
+            num += (c - 'A') + 1;
         }
         return num;
     }
 };
-
-int main()
-{
-    Solution solu;
-    int result1 = solu.titleToNumber("B");
-    int result2 = solu.titleToNumber("AB");
-    cout << result1 << '\n' << result2 << endl;
-
-    return 0;
-}
