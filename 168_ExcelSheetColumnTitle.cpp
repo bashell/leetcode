a78d2a70f0d998c1771214cd4c136e4bb1166256
bashell <nju.liuc@gmail.com>
2016-04-02 11:54:34 +0800

    Math

--- 168_ExcelSheetColumnTitle.cpp
+++ 168_ExcelSheetColumnTitle.cpp
@@ -1,26 +1,11 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-class Solution{
+class Solution {
 public:
-    string convertTotile(int n) {
+    string convertToTitle(int n) {
         string str;
         for( ; n > 0; n /= 26) {
-            n -= 1;
-            str = (char)('A' + n % 26) + str;    // 由于字符是从右向左得到的
+            --n;
+            str = (char)('A' + n % 26) + str;    // 字符是从右向左获得
         }
         return str;
     }
 };
-
-int main()
-{
-    Solution solu;
-    string str1 = solu.convertTotile(24);
-    string str2 = solu.convertTotile(26);
-    string str3 = solu.convertTotile(28);
-    cout << str1 << '\n' << str2 << '\n' << str3 << endl;
-
-    return 0;
-}
