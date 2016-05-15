4c5cd68c456dd0559536abafbc69c6a59c2a9818
bashell <nju.liuc@gmail.com>
2016-05-15 13:45:47 +0800

    Tree

---
+++ 236_LowestCommonAncestorOfABinaryTree.cpp
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(p == root || q == root || root == NULL)
+            return root;
+        TreeNode *left = lowestCommonAncestor(root->left, p, q);
+        TreeNode *right = lowestCommonAncestor(root->right, p, q);
+        if(left && right)  // p和q分别落于root的左右两个subtree中
+            return root;
+        return left ? left : right;
+    }
+};
