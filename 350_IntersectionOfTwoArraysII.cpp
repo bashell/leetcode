e8b90134751d0a1504e23f4669ce64fb5ae9e0dc
bashell <nju.liuc@gmail.com>
2016-06-27 11:50:24 +0800

    Hash Table

--- 350_IntersectionOfTwoArraysII.cpp
+++ 350_IntersectionOfTwoArraysII.cpp
@@ -1,3 +1,4 @@
+// Solution1: map
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
@@ -9,6 +10,29 @@ public:
             if(imap.find(i) != imap.end() && imap[i] > 0) {
                 --imap[i];
                 res.push_back(i);
+            }
+        }
+        return res;
+    }
+};
+
+// Solution2: two pointers
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> res;
+        if(nums1.size() == 0 || nums2.size() == 0) return res;
+        std::sort(nums1.begin(), nums1.end());
+        std::sort(nums2.begin(), nums2.end());
+        int m = nums1.size(), n = nums2.size();
+        for(int i = 0, j = 0; i < m && j < n; ) {
+            if(nums1[i] == nums2[j]) {
+                res.push_back(nums1[i]);
+                ++i, ++j;
+            } else if(nums1[i] < nums2[j]) {
+                ++i;
+            } else {
+                ++j;
             }
         }
         return res;
