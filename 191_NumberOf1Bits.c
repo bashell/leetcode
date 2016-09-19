25aa67e1fcf3693fb206fa1ed21ac6a71fec8103
bashell <nju.liuc@gmail.com>
2016-09-19 15:39:38 +0800

    Bit Manipulation

--- 191_NumberOf1Bits.c
+++ 191_NumberOf1Bits.c
@@ -10,3 +10,13 @@ int hammingWeight(uint32_t n) {
     }
     return result; 
 }
+
+
+int hammingWeight(uint32_t n) {
+    int cnt = 0;
+    while(n) {
+        ++cnt;
+        n = n&(n-1);
+    }
+    return cnt;
+}
--- 231_PowerOfTwo.c
+++ 231_PowerOfTwo.c
@@ -7,3 +7,14 @@ bool isPowerOfTwo(int n) {
     }
     return count == 1;
 }
+
+
+bool isPowerOfTwo(int n) {
+    if(n <= 0) return false;
+    int count = 0;
+    while(n) {
+        ++count;
+        n = n&(n-1);
+    }
+    return count == 1;
+}
